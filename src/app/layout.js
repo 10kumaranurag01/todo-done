ff3fdaa507a760bec11e36532720c3db6de292cb
Xron Trix <xrontrix@gmail.com>
2024-10-03 23:41:23 +0530

    refactor: moved fonts to src
    fonts should not be placed inside app directory as it is used for routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,12 +9,12 @@ import { LoadingProvider } from "@/lib/context/LoadingContext";
 import { ThemeProvider } from "@/components/Providers/ThemeProvider";
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
