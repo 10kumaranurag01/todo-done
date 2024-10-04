b4127c2afcb67f852eda092e8cef1627c8ea8ff9
Xron Trix <xrontrix@gmail.com>
2024-10-04 23:48:39 +0530

    refactor: moved fonts inside assets

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,12 +10,12 @@ import { ThemeProvider } from "@/components/Providers/ThemeProvider";
 import { AuthProvider } from "@/lib/context/Auth.context";
 
 const geistSans = localFont({
-  src: "../fonts/GeistVF.woff",
+  src: "../assets/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "../fonts/GeistMonoVF.woff",
+  src: "../assets/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
