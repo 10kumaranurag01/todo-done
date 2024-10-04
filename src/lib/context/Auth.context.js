3330649f88a0c38279e99c7b674182c960a47f2e
Xron Trix <xrontrix@gmail.com>
2024-10-04 23:11:20 +0530

    feat: ensuring auth context is used within auth provider

--- src/lib/context/Auth.context.js
+++ src/lib/context/Auth.context.js
@@ -42,4 +42,10 @@ export const AuthProvider = ({ children }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext);
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
