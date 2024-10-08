02be5fdf85fd88c1d420675c212d243a5ff92db0
krishna3451 <fkrishna1729@gmail.com>
2024-10-08 23:01:50 +0530

    The selected section in the sidebar is  darker to visually indicate that it is currently active.

--- src/components/nav/SideMenu.js
+++ src/components/nav/SideMenu.js
@@ -11,15 +11,15 @@ const SideMenu = ({
       <div className="flex h-full max-h-screen flex-col gap-2">
         <nav className="grid items-start px-1 text-sm font-medium lg:px-4 mt-4 ">
           <Button
-            className={`w-full text-left py-2 px-4 `}
-            variant={`${selectedPage === "todos" ? "secondary" : "default"}`}
+            className={`w-full text-left py-2 px-4 ${selectedPage === "todos" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}
+            variant="default"
             onClick={() => setSelectedPage("todos")}
           >
             # To-Dos List
           </Button>
           <Button
-            className={`w-full text-left py-2 px-4 mt-2`}
-            variant={`${selectedPage === "kanban" ? "secondary" : "default"}`}
+            className={`w-full text-left py-2 px-4 mt-2 ${selectedPage === "kanban" ? "bg-gray-900 text-white" : "bg-gray-100 text-gray-900"}`}
+            variant="default"
             onClick={() => setSelectedPage("kanban")}
           >
             # Kanban Board
